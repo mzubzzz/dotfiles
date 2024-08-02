c6939d51cccfd05d31207f8e871225038dc4b2cb
mzubzzz <zubarev.mihail@gmail.com>
2024-08-02 04:48:25 +0300

    nvim show full error remap. webpmtSsh new server

--- nvim/lua/mzubzzz/remap.lua
+++ nvim/lua/mzubzzz/remap.lua
@@ -1,2 +1,5 @@
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+local bufopts = { noremap = true, silent = true }
+vim.keymap.set("n", "<leader>fe", vim.diagnostic.open_float, bufopts)
